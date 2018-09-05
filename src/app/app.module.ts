88518f2ea2f61f29207b2488948432bd329c78f1
Javier Santa Cruz <2010204javiergomez@gmail.com>
2018-09-05 16:54:22 -0600

    App terminada

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,20 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 
+import * as firebase from 'firebase';
+
+const config = {
+  apiKey: "AIzaSyDBSR_jZ41mYxm1fO3uom43pPc1XvPCcWw",
+  authDomain: "example-12c6e.firebaseapp.com",
+  databaseURL: "https://example-12c6e.firebaseio.com",
+  projectId: "example-12c6e",
+  storageBucket: "",
+  messagingSenderId: "880600828926",
+  timestampsInSnapshots: true
+};
+
+firebase.initializeApp(config);
+
 @NgModule({
   declarations: [
     MyApp,
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,119 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  messages : any;
+  private db = firebase.firestore();
+  model : any ={};
+  isEditing : boolean = false;
 
+  constructor(public navCtrl: NavController) {
+    this.db = firebase.firestore();
+    this.loadData();
+  }
+
+  loadData(){
+    this.getAllDocuments("messages").then((e) => {
+      this.messages = e;
+    })
+  }
+
+  addMessage(){
+    if(!this.isEditing){
+      this.addDocument("messages", this.model).then(()=> {
+        this.loadData();
+      });
+    }else{
+      this.updateDocument("messages", this.model.$key, this.model).then(() => {
+        this.loadData();
+      })
+    }
+
+    this.isEditing = false;
+    this.model.title = "";
+    this.model.text = ""; 
+  }
+
+  updateMessage(obj){
+    this.model = obj;
+    this.isEditing = true;
+  }
+
+  deleteMessage(key){
+    this.deleteDocument("message", key).then(()=>{
+      this.isEditing = false;
+      this.loadData();
+    })
+  }
+
+  getAllDocuments(collection : string){
+    return new Promise((resolve, reject) => {
+      this.db.collection(collection)
+      .get()
+      .then((querySnapShot) => {
+        let arr = [];
+        querySnapShot.forEach(function(doc){
+          var obj = JSON.parse(JSON.stringify(doc.data()));
+          obj.$key = doc.id
+          //console.log(obj)
+          arr.push(obj);
+        });
+
+        if(arr.length > 0){
+          //console.log("Document data: ", arr);
+          resolve(arr);
+        }else{
+          //console.log("No such document");
+          resolve(null);
+        }
+      })
+      .catch((error : any) => {
+        reject(error);
+      });
+    });
+  }
+
+  addDocument(collectionName : string, dataObj : any) : Promise<any>{
+    return new Promise((resolve, reject) => {
+      this.db.collection(collectionName).add(dataObj)
+      .then((obj : any) => {
+        resolve(obj);
+      })
+      .catch((error : any) => {
+        reject(error);
+      });
+    });
+  }
+
+  updateDocument(collectionName: string, docId: string, dataObj: any) : Promise<any>{
+    return new Promise((resolve, reject) => {
+      this.db
+          .collection(collectionName)
+          .doc(docId)
+          .update(dataObj)
+          .then((obj : any) => {
+            resolve(obj);
+            console.log(collectionName);
+            console.log(docId);
+          })
+          .catch((error : any) => {
+            reject(error);
+          })
+    })
+  }
+
+  deleteDocument(collectionName: string, docID: string): Promise<any> {
+    return new Promise((resolve, reject) => {
+        console.log(collectionName);
+        this.db.doc('messages/'+ docID)
+            .delete()
+            .then((obj: any) => {
+                console.log('Item deleted successfully')
+                resolve(obj);
+            })
+            .catch((error: any) => {
+                reject(error);
+            });
+    });
   }
 
 }
